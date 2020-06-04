ef8fd70b14b8d7fce4b0712464575bd58d038917
OtherwiseJLo <otherwisejlo@gmail.com>
2020-06-04 12:28:47 -0400

    Getters return each row

--- src/TicTacToeBoard.py
+++ src/TicTacToeBoard.py
@@ -2,6 +2,7 @@ import numpy as np
 
 class TicTacToeBoard:
     def __init__(self, n=3):
+        self.size = n
         blank_board = [' ']*n*n
         self.board = np.array(blank_board).reshape([n,n])
         
@@ -10,11 +11,13 @@ class TicTacToeBoard:
         self.diagonals = self._get_diagonals()
     
     def _get_rows(self):
-        pass
+        return [''.join(self.board[i, :]) for i in range(self.size)]
         
     def _get_columns(self):
-        pass
+        return [''.join(self.board[:, i]) for i in range(self.size)]
         
     def _get_diagonals(self):
-        pass
+        top_left_bottom_right = self.board.diagonal()
+        top_right_bottom_left = np.fliplr(self.board).diagonal()
+        return [''.join(diag) for diag in [top_left_bottom_right, top_right_bottom_left]]
         
