026f8cb5e8f4f41dd0052a0210d0115b5107ae56
OtherwiseJLo <otherwisejlo@gmail.com>
2020-06-04 12:21:33 -0400

    Created getters for all row types

--- src/TicTacToeBoard.py
+++ src/TicTacToeBoard.py
@@ -5,3 +5,16 @@ class TicTacToeBoard:
         blank_board = [' ']*n*n
         self.board = np.array(blank_board).reshape([n,n])
         
+        self.rows = self._get_rows()
+        self.columns = self._get_columns()
+        self.diagonals = self._get_diagonals()
+    
+    def _get_rows(self):
+        pass
+        
+    def _get_columns(self):
+        pass
+        
+    def _get_diagonals(self):
+        pass
+        
